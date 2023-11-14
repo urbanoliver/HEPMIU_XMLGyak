6361bed71518c09d40ac71636315e5de6ee97832
urbanoliver <90559472+urbanoliver@users.noreply.github.com>
2023-11-14 13:59:58 +0300

    Add files via upload

--- XMLTaskHEPMIU/1.feladat/DomModifyHepmiu.java
+++ XMLTaskHEPMIU/1.feladat/DomModifyHepmiu.java
@@ -6,6 +6,7 @@ import javax.xml.transform.*;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
 import java.io.File;
+import java.util.Scanner;
 
 public class DomModifyHepmiu {
 	public static void main(String[] args) {
@@ -16,31 +17,61 @@ public class DomModifyHepmiu {
 			DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
 			Document doc = dBuilder.parse(xmlFile);
 			doc.getDocumentElement().normalize();
+			
+			Scanner sc = new Scanner(System.in);
+			
+			System.out.println("Adja meg az element nevet!");
+			String e_Nev = sc.nextLine();
+			System.out.println("Adja meg az element ID-jat!");
+			String e_ID = sc.nextLine();
+			System.out.println("Adja meg a modositani kivant attributumot!");
+			String e_Attrib = sc.nextLine();
+			System.out.println("Adja meg az attributum uj erteket!");
+			String e_UjErtek = sc.nextLine();
 
-			// Find the node that you want to modify
-			NodeList nodeList = doc.getElementsByTagName("your_tag_name");
-
-			for (int i = 0; i < nodeList.getLength(); i++) {
-				Node node = nodeList.item(i);
-
-				if (node.getNodeType() == Node.ELEMENT_NODE) {
-					Element element = (Element) node;
-					// Modify the elements as needed
-					element.getElementsByTagName("name").item(0).setTextContent("New Name");
-					element.getElementsByTagName("value").item(0).setTextContent("New Value");
-					// Add more lines for other elements as needed
-				}
-			}
+			// Modify attributes and values based on element ID
+			modifyAttributesByID(doc,e_Nev, e_ID, e_Attrib, e_UjErtek);
 
 			// Write the updated content to the same XML file
+			writeToFile(doc, "your_modified_xml_file.xml");
+
+			System.out.println("Attributes and values modified successfully.");
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	// Modify attributes and values based on element ID
+	public static void modifyAttributesByID(Document doc,String elementName, String elementID, String attributeName, String newValue) {
+		NodeList nodeList = doc.getElementsByTagName(elementName);
+
+		for (int i = 0; i < nodeList.getLength(); i++) {
+			Node node = nodeList.item(i);
+
+			if (node.getNodeType() == Node.ELEMENT_NODE) {
+				Element element = (Element) node;
+				// Check if the element has the desired ID
+				if (element.getAttribute("id").equals(elementID)) {
+					// Modify attributes and values
+					element.setAttribute("your_attribute_name", attributeName);
+					element.getElementsByTagName("your_value_element").item(0).setTextContent(newValue);
+					// Add more lines for other attributes or values as needed
+				}
+			}
+		}
+	}
+
+	// Write the updated content to a new XML file
+	public static void writeToFile(Document doc, String filename) {
+		try {
 			TransformerFactory transformerFactory = TransformerFactory.newInstance();
 			Transformer transformer = transformerFactory.newTransformer();
+			transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+
 			DOMSource source = new DOMSource(doc);
-			StreamResult result = new StreamResult(new File("XMLHepmiu.xml"));
+			StreamResult result = new StreamResult(new File(filename));
 			transformer.transform(source, result);
-
-			System.out.println("XML file updated successfully.");
-
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
--- XMLTaskHEPMIU/1.feladat/DomQueryHepmiu.java
+++ XMLTaskHEPMIU/1.feladat/DomQueryHepmiu.java
@@ -3,6 +3,7 @@ package hu.domparse.hepmiu;
 import org.w3c.dom.*;
 import javax.xml.parsers.*;
 import java.io.File;
+import java.util.Scanner;
 
 public class DomQueryHepmiu {
 
@@ -10,28 +11,77 @@ public class DomQueryHepmiu {
 		try {
 			File xmlFile = new File("XMLHepmiu.xml");
 
-			DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();					//SZUKSEGES DOM OSZTALYOK PELDANYOSITASA
+			DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance(); // SZUKSEGES DOM OSZTALYOK
+																						// PELDANYOSITASA
 			DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
 			Document doc = dBuilder.parse(xmlFile);
 			doc.getDocumentElement().normalize();
 
+			Scanner sc = new Scanner(System.in);
+
+			System.out.println("Adja meg az element nevet!");
+			String e_Nev = sc.nextLine();
+			System.out.println("Adja meg az element ID-janak nevet!");
+			String e_ID_Nev = sc.nextLine();
+			System.out.println("Adja meg az element ID-janak erteket!");
+			String e_ID_Ertek = sc.nextLine();
+
+			getElementByID(doc, 0, "", e_Nev, e_ID_Nev, e_ID_Ertek);
+
+			sc.close();
 			// Query for elements based on a specific tag name
-			NodeList nodeList = doc.getElementsByTagName("your_tag_name");
 
-			for (int i = 0; i < nodeList.getLength(); i++) {
-				Node node = nodeList.item(i);
-
-				if (node.getNodeType() == Node.ELEMENT_NODE) {											//XML ELEMENT LEKEERDEZESE TAG NEV ALAPJAN
-					Element element = (Element) node;
-					// Access elements within the node and retrieve data
-					String data = element.getElementsByTagName("specific_element").item(0).getTextContent();
-					System.out.println("Retrieved Data: " + data);										//LEKERDEZETT ELEMENT KIIRASA
-					// Add more lines for other elements or attributes as needed
-				}
-			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+	}
+
+	public static void getElementByID(Document doc, int level, String indent, String elementName, String elementID_Name,
+			String elementID) {
+
+		NodeList nodeList = doc.getElementsByTagName(elementName);
+
+		for (int i = 0; i < nodeList.getLength(); i++) {
+			Node node = nodeList.item(i);
+			Element element = (Element) node;
+
+			if (element.getAttribute(elementID_Name).equals(elementID)) {
+				// Check if the element has the desired ID
+				Element keresett_Elem = doc.getDocumentElement();
+				kiirTartalom(keresett_Elem, "");
+			}
+			// Add more lines for other attributes or values as needed
+		}
+	}
+
+	public static String getIndent(int level) { // FA STRUKTURA LETREHOZASA
+		StringBuilder indent = new StringBuilder();
+		for (int i = 0; i < level; i++) {
+			indent.append("  ");
+		}
+		return indent.toString();
+	}
+
+	private static void kiirTartalom(Node node, String indent) {
+		if (node.getNodeType() == Node.ELEMENT_NODE) {
+			System.out.println(indent + node.getNodeName());
+
+			if (node.hasAttributes()) {
+				NamedNodeMap attrib = node.getAttributes();
+				for (int i = 0; i < attrib.getLength(); i++) {
+					Node attribute = attrib.item(i);
+					System.out.println(indent + attribute.getNodeName() + " = " + attribute.getNodeValue());
+				}
+			}
+
+			if (node.hasChildNodes()) {
+				NodeList gyerek = node.getChildNodes();
+				for (int i = 0; i < gyerek.getLength(); i++) {
+					Node child = gyerek.item(i);
+					kiirTartalom(child, indent + "  ");
+				}
+			}
+		} 
 	}
 
 }

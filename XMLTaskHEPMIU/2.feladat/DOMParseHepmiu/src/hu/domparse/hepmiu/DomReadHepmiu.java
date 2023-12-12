92ca50bb7090fc142920c6a9b91e343096bcd9f2
urbanoliver <90559472+urbanoliver@users.noreply.github.com>
2023-12-12 10:58:50 +0100

    Add files via upload

--- XMLTaskHEPMIU/2.feladat/DOMParseHepmiu/src/hu/domparse/hepmiu/DomReadHepmiu.java
+++ XMLTaskHEPMIU/2.feladat/DOMParseHepmiu/src/hu/domparse/hepmiu/DomReadHepmiu.java
@@ -1,58 +1,111 @@
 package hu.domparse.hepmiu;
 
-import org.w3c.dom.*;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import javax.xml.parsers.*;
-import java.io.File; //VALTOZTATAS SZUKSEGES 
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import org.w3c.dom.*;
+import org.xml.sax.SAXException;
 
 public class DomReadHepmiu {
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	// TODO Auto-generated method stub
 		try {
+			File newXMLFile = new File("XMLHepmiu.xml");
+			StreamResult newXmlStream = new StreamResult(newXMLFile);
 
-			File xmlFile = new File("XMLHepmiu.xml");
+			DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder builder = factory.newDocumentBuilder();
+			Document doc = builder.parse(new File("./XMLHepmiu.xml"));
+			cutEmptyStrings(doc.getDocumentElement());
 
-			DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-			DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-			Document doc = dBuilder.parse(xmlFile);
-			doc.getDocumentElement().normalize();
+			writeDoc(doc, newXmlStream);
 
-			Element gyokerElem = doc.getDocumentElement();
-			System.out.println("Gyökér elem: " + gyokerElem.getNodeName());
-
-			kiirTartalom(gyokerElem, "");
+			System.out.println(makeToXMLFormat(doc));
 
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-
 	}
 
-	private static void kiirTartalom(Node node, String indent) {
-		if (node.getNodeType() == Node.ELEMENT_NODE) {
-			System.out.println(indent + node.getNodeName());
-
-			if (node.hasAttributes()) {
-				NamedNodeMap attrib = node.getAttributes();
-				for (int i = 0; i < attrib.getLength(); i++) {
-					Node attribute = attrib.item(i);
-					System.out.println(indent + attribute.getNodeName() + " = " + attribute.getNodeValue());
-				}
-			}
-
-			if (node.hasChildNodes()) {
-				NodeList gyerek = node.getChildNodes();
-				for (int i = 0; i < gyerek.getLength(); i++) {
-					Node child = gyerek.item(i);
-					kiirTartalom(child, indent + "  ");
-				}
-			}
-		} else if (node.getNodeType() == Node.TEXT_NODE) {
-			String datas = node.getNodeValue().trim();
-			if (!datas.isEmpty()) {
-				System.out.println(indent + datas);
+	// A fájlban lévõ üres stringek törlése
+	private static void cutEmptyStrings(Node root) {
+		NodeList nodeList = root.getChildNodes();
+		List<Node> deleteEmptyLists = new ArrayList<>();
+		for (int i = 0; i < nodeList.getLength(); i++) {
+			if (nodeList.item(i).getNodeType() == Node.TEXT_NODE
+					&& nodeList.item(i).getTextContent().strip().isEmpty()) {
+				deleteEmptyLists.add(nodeList.item(i));
+			} else {
+				cutEmptyStrings(nodeList.item(i));
 			}
 		}
+		for (Node node : deleteEmptyLists) {
+			root.removeChild(node);
+		}
+	}
+
+	// Új XML fájlba írása
+	public static void writeDoc(Document document, StreamResult output) {
+		try {
+			TransformerFactory transformerFactory = TransformerFactory.newInstance();
+			Transformer transformer = transformerFactory.newTransformer();
+			transformer.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
+			transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+			DOMSource source = new DOMSource(document);
+			transformer.transform(source, output);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	// XML formátum létrehozása
+	public static String makeToXMLFormat(Document document) {
+		return "<?xml version=\"" + document.getXmlVersion() + "\" encoding=\"" + document.getXmlEncoding() + "\" ?>\n"
+				+ elementsToXMLFormat(document.getDocumentElement(), 0);
+	}
+
+	// Tag-ek rendelése az XML elementekhez
+	public static String elementsToXMLFormat(Node node, int indent) {
+		if (node.getNodeType() != Node.ELEMENT_NODE) {
+			return "";
+		}
+		StringBuilder output = new StringBuilder();
+		output.append(getIndent(indent)).append("<").append(((Element) node).getTagName());
+		if (node.hasAttributes()) {
+			for (int i = 0; i < node.getAttributes().getLength(); i++) {
+				Node attribute = node.getAttributes().item(i);
+				output.append(" ").append(attribute.getNodeName()).append("=\"").append(attribute.getNodeValue())
+						.append("\"");
+			}
+		}
+		NodeList children = node.getChildNodes();
+		if (children.getLength() == 1 && children.item(0).getNodeType() == Node.TEXT_NODE) {
+			output.append(">").append(children.item(0).getTextContent().trim()).append("</")
+					.append(((Element) node).getTagName()).append(">\n");
+		} else {
+			output.append(">\n");
+			for (int i = 0; i < children.getLength(); i++) {
+				output.append(elementsToXMLFormat(children.item(i), indent + 1));
+			}
+			output.append(getIndent(indent)).append("</").append(((Element) node).getTagName()).append(">\n");
+		}
+		return output.toString();
+	}
+
+	// Space-ek beiktatása
+	private static String getIndent(int indent) {
+		StringBuilder indentation = new StringBuilder();
+		for (int i = 0; i < indent; i++) {
+			indentation.append("    ");
+		}
+		return indentation.toString();
 	}
 
 }
